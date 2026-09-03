812f6e33a9442b235132ed238f2dda4956d9eb98
Sagnik Chandra <chandrasagnik2004@gmail.com>
2026-09-04 03:57:29 +0530

    feat(avatar_voice): implement AvatarFactory supporting auto, tier1, and tier2 resolution

--- modules/avatar_voice/src/avatar/__init__.py
+++ modules/avatar_voice/src/avatar/__init__.py
@@ -5,9 +5,13 @@ Avatar Subsystem package exports.
 from modules.avatar_voice.src.avatar.base import AvatarAdapter
 from modules.avatar_voice.src.avatar.viseme_avatar import VisemeAvatarAdapter
 from modules.avatar_voice.src.avatar.wav2lip_avatar import Wav2LipAvatarAdapter
+from modules.avatar_voice.src.avatar.musetalk_avatar import MuseTalkAvatarAdapter
+from modules.avatar_voice.src.avatar.factory import AvatarFactory
 
 __all__ = [
     "AvatarAdapter",
     "VisemeAvatarAdapter",
     "Wav2LipAvatarAdapter",
+    "MuseTalkAvatarAdapter",
+    "AvatarFactory",
 ]
